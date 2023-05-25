3c660ff444a58d14463736543dacd15a96869be2
kacperpy <kacperpy@gmail.com>
2023-05-26 00:31:23 +0200

    added portfolio page with images loading

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -39,10 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'images'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,7 @@ MEDIA_ROOT = BASE_DIR / "mediafiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
--- portfolio_frontend/src/pages/portfolioPage/PortfolioPage.tsx
+++ portfolio_frontend/src/pages/portfolioPage/PortfolioPage.tsx
@@ -1,10 +1,84 @@
-import { Container, Typography } from "@mui/material";
+import { SetStateAction, useEffect, useState } from "react";
+import {
+  Container,
+  ImageList,
+  ImageListItem,
+  Box,
+  CircularProgress,
+  Dialog,
+} from "@mui/material";
+import axios from "axios";
 import styles from "./PortfolioPage.module.css";
 
+interface Image {
+  uuid: string;
+  created_at: string;
+  media_file: string;
+  filter: string;
+}
+
 export const PortfolioPage = () => {
+  const [images, setImages] = useState<Image[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [selectedImage, setSelectedImage] = useState<Image | null>(null);
+
+  useEffect(() => {
+    axios
+      .get<Image[]>("http://localhost:8000/api/images/")
+      .then((response: { data: SetStateAction<Image[]> }) => {
+        setImages(response.data);
+        setIsLoading(false);
+      })
+      .catch((error: any) => {
+        console.error("Error fetching images:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const handleImageClick = (image: Image) => {
+    setSelectedImage(image);
+  };
+
+  const handleClose = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <Container className={styles.pageContainer}>
-      <Typography>Portfolio Page</Typography>
+      {isLoading ? (
+        <Box
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height={200}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box width="100%" height="100%">
+          <ImageList variant="quilted" cols={4} gap={20}>
+            {images.map((image) => (
+              <ImageListItem key={image.uuid} className={styles.imageListItem}>
+                <img
+                  src={image.media_file}
+                  alt={image.created_at}
+                  onClick={() => handleImageClick(image)}
+                  className={styles.image}
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        </Box>
+      )}
+      <Dialog open={selectedImage !== null} onClose={handleClose} maxWidth="md">
+        {selectedImage && (
+          <img
+            src={selectedImage.media_file}
+            alt={selectedImage.created_at}
+            style={{ width: "100%", height: "auto" }}
+          />
+        )}
+      </Dialog>
     </Container>
   );
 };
