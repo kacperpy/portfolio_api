8ca9c16599ba3642f24eca478fa8befc63386d9e
kacperpy <kacperpy@gmail.com>
2023-11-21 01:39:40 +0100

    updated mobile version

--- nginx/portfolio_frontend/src/App.tsx
+++ nginx/portfolio_frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles/App.css";
 import { Route, Routes } from "react-router-dom";
 import { createCustomTheme } from "./utils/createCustomTheme";
@@ -9,15 +9,34 @@ import { PortfolioPage } from "./pages/portfolioPage/PortfolioPage";
 import { LoginPage } from "./pages/loginPage/LoginPage";
 import { ClientPage } from "./pages/clientPage/ClientPage";
 import { AboutPage } from "./pages/aboutPage/AboutPage";
+import { TopBarMobile } from "./components/topBar/TopBarMobile";
 // import { SideBar } from "./components/sideBar/SideBar";
 
 function App() {
   const theme = createCustomTheme();
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 1400);
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Remove event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty dependency array means this effect will run once on mount
 
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <TopBar />
+        {isMobile ? <TopBarMobile /> : <TopBar />}
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="/portfolio" element={<PortfolioPage />}></Route>
