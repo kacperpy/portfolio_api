ff1803771b0ac9ca4db5ae17801ed549d2be5e7e
kacperpy <kacperpy@gmail.com>
2023-11-27 15:28:02 +0100

    admin update

--- images/admin.py
+++ images/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from images.models import Client, Filter, Image
+from images.models import Client, Filter, Image, VideoClip
 
 admin.site.register(Image)
+admin.site.register(VideoClip)
 admin.site.register(Filter)
 admin.site.register(Client)
