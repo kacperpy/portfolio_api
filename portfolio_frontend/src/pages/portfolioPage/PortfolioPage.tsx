20a378a63ce28b98e99f0287eb5ccc2859481a31
kacperpy <kacperpy@gmail.com>
2023-11-27 15:19:59 +0100

    video player

--- portfolio_frontend/src/pages/portfolioPage/PortfolioPage.tsx
+++ portfolio_frontend/src/pages/portfolioPage/PortfolioPage.tsx
@@ -1,10 +1,18 @@
-import { Container, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import styles from "./PortfolioPage.module.css";
+import ReactPlayer from "react-player";
 
 export const PortfolioPage = () => {
   return (
-    <Container className={styles.portfolioPageContainer}>
-      <Typography>Portfolio Page</Typography>
-    </Container>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+      }}
+    >
+      <ReactPlayer url={"https://vimeo.com/888695664?share=copy"} controls />
+    </Box>
   );
 };
