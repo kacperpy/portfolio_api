2c8848be76c6b179e50376570a688a6c91d7937f
kacperpy <kacperpy@gmail.com>
2024-01-21 15:16:57 +0100

    restored video is_landscape

--- images/api/serializers.py
+++ images/api/serializers.py
@@ -45,6 +45,7 @@ class VideoClipReadSerializer(serializers.ModelSerializer):
     filter = serializers.SerializerMethodField()
     client = serializers.SerializerMethodField()
     file_name = serializers.SerializerMethodField()
+    is_landscape = serializers.SerializerMethodField()
 
     class Meta:
         model = Image
@@ -56,6 +57,7 @@ class VideoClipReadSerializer(serializers.ModelSerializer):
             'client',
             'file_name',
             'caption',
+            'is_landscape'
         ]
 
     def get_created_at(self, instance):
@@ -69,3 +71,6 @@ class VideoClipReadSerializer(serializers.ModelSerializer):
 
     def get_file_name(self, instance):
         return instance.media_file.name
+
+    def get_is_landscape(self, instance):
+        return instance.media_file.width > instance.media_file.height
